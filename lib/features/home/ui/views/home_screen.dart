675f17de93ee2172df680bfc29de474ad572f492
KhaledHAnter <khaldntr11@gmail.com>
2024-11-19 02:45:32 +0200

    - add localization - add arabic lacale

--- lib/features/home/ui/views/home_screen.dart
+++ lib/features/home/ui/views/home_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tps/generated/l10n.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
           child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Text(S.of(context).hello),
+        ],
       )),
     );
   }
