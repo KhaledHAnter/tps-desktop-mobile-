f9de3efb7b4835a7df408e8cacfc4be8ab2b54e4
KhaledHAnter <khaldntr11@gmail.com>
2024-11-21 04:47:33 +0200

    - link firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tps/core/routing/app_router.dart';
 import 'package:tps/tps.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Tps(
     appRouter: AppRouter(),
   ));
