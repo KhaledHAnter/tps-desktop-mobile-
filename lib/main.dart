3d80b2ab7d065d6272cbba7e8d9c191b9f273055
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-11-24 23:55:05 +0200

    add options to firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tps/firebase_options.dart';
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 import 'tps.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupGetIt();
   runApp(Tps(
     appRouter: AppRouter(),
