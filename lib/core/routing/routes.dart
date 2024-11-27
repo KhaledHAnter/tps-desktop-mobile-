0cc09739f79eb5ba121fa15c355b213a41e82590
KhaledHAnter <khaldntr11@gmail.com>
2024-11-27 21:48:59 +0200

    - add local auth service to the project. - fix the WhatsApp link bug.

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
-  static const String homeScreen = '/';
+  static const String localAuthScreen = '/';
+  static const String homeScreen = '/homeScreen';
   static const String palyerDetailsScreen = '/palyerDetailsScreen';
 }
