f1ab0a1a3d27d28c3162e1fb07b9b820c58331bd
KhaledHAnter <khaldntr11@gmail.com>
2025-04-24 17:50:03 +0200

    wrap the charts inside gride view to make it responsive with web

--- lib/features/player_exercises/ui/views/exercise_statistics_screen.dart
+++ lib/features/player_exercises/ui/views/exercise_statistics_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:developer';
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:tps/core/widgets/gray_container.dart';
 import 'package:tps/features/player_exercises/data/models/exercise_model.dart';
 import 'package:tps/features/player_exercises/ui/widgets/build_exercise_chart.dart';
 import 'package:tps/features/player_exercises/ui/widgets/statistics_header.dart';
@@ -14,33 +13,46 @@ class ExerciseStatisticsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    log(allExercises.length.toString());
+    final double width = MediaQuery.sizeOf(context).width;
+
+    final int crossAxisCount = width < 500 ? 1 : width ~/ 350;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
+          child: Column(
             children: [
               const StatisticsHeader(),
               const Gap(16),
-              ...allExercises.map(
-                (exercise) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(exercise.name,
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 10),
-                      AspectRatio(
-                        aspectRatio: 1.7,
-                        child: LineChart(
-                          buildChart(exercise),
-                        ),
-                      ),
-                    ],
+              Expanded(
+                child: GridView.builder(
+                  itemCount: allExercises.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    childAspectRatio: 1.5, // Adjust as needed
                   ),
+                  itemBuilder: (context, index) {
+                    final exercise = allExercises[index];
+                    return GrayContainer(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            exercise.name,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          Expanded(
+                            child: LineChart(buildChart(exercise)),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
