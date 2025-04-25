f491e2c41ecab4e967ba13b86aefe656d2ecea86
KhaledHAnter <khaldntr11@gmail.com>
2025-04-25 20:58:04 +0300

    back to mobile version

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,8 +1,8 @@
 class Routes {
-  static const String localAuthScreen = '/localAuthScreen';
-  // static const String localAuthScreen = '/';
-  static const String authScreen = '/';
-  // static const String authScreen = '/authScreen';
+  // static const String localAuthScreen = '/localAuthScreen';
+  static const String localAuthScreen = '/';
+  // static const String authScreen = '/';
+  static const String authScreen = '/authScreen';
   static const String homeScreen = '/homeScreen';
   static const String palyerDetailsScreen = '/palyerDetailsScreen';
   static const String palyerExercisesScreen = '/palyerExercisesScreen';
