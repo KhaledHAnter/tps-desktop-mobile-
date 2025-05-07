5e2a1aa449dc9bd047fa449be2eaf424916f5192
KhaledHAnter <khaldntr11@gmail.com>
2025-05-07 17:37:56 +0300

    configure the files to publish an apk

--- lib/features/home/ui/views/widgets/player_section.dart
+++ lib/features/home/ui/views/widgets/player_section.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:tps/core/helpers/extentions.dart';
 import 'package:tps/core/routing/routes.dart';
@@ -8,9 +10,47 @@ import 'player_tile.dart';
 class PlayersSection extends StatelessWidget {
   final List<PlayerModel> players;
   final bool showMoney;
+  static List<String> names = [
+    'خالد عنتر',
+    'محمد البسيوني',
+    'احمد عبدالله',
+    'زياد الحسيني',
+    'طارق رجب',
+    'ادهم خالد',
+    'محمد محمود',
+    'طه محمد',
+    'محمود عادل',
+    'كريم الظابط'
+  ];
+  static List<String> sports = [
+    'سباحة',
+    'كرة القدم',
+    'كرة السلة',
+    'كياك',
+    'بوكسينج',
+  ];
+  static List<PlayerModel> dummyPlayers = List.generate(
+    10,
+    (index) => PlayerModel(
+      shouldShowReminder: false,
+      age: index + 20,
+      description: null,
+      endDate: DateTime.now(),
+      freeze: null,
+      money: (index + 1) * 600,
+      name: names[index],
+      phase: 'under_exam1',
+      phone: '201012343087',
+      remainingDuration: 0,
+      startDate: DateTime.now(),
+      sport: Random().nextBool()
+          ? sports[Random().nextInt(sports.length)]
+          : 'null',
+      subsDuration: Random().nextInt(10) + 1,
+    ),
+  );
   const PlayersSection(
       {super.key, required this.players, required this.showMoney});
-
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
